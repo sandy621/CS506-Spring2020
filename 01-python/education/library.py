de4a0d867286501f262ff5b3152b3321fa0c5384
Sandy Seedhom <sandy621@bu.edu>
2020-01-27 17:21:45 -0500

    Sandy and Nada added library to city

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,10 @@
 def draw_library():
-    print("library not found")
+    print("__________________")
+    print("|                |")
+    print("| PUBLIC LIBRARY |")
+    print("|                |")
+    print("|       __       |")
+    print("|      /  \      |")
+    print("------------------")
+    print()
     return
